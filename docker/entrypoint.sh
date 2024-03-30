eb3e88649d31a9260f627e651a382517ed9f649d
Vicram <vicramanjohfue@gmail.com>
2024-03-30 18:02:10 +0200

    update 16

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,47 +1,65 @@
 #!/bin/bash
 
-if [ ! -f "vendor/autoload.php" ]; then
-    composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader
-fi
+VERSION=""
 
-if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
-    cp .env.example .env
-    case "$APP_ENV" in
-    "local")
-        echo "Copying .env.example ... "
-        cp .env.example .env
-    ;;
-    "prod")
-        echo "Copying .env.prod ... "
-        cp .env.prod .env
-    ;;
-    esac
+# get parameters
+while getopts v: flag
+do
+  case "${flag}" in
+    v) VERSION=${OPTARG};;
+  esac
+done
+
+# get highest tag number, and add v0.1.0 if doesn't exist
+git fetch --prune --unshallow 2>/dev/null
+CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+
+if [[ $CURRENT_VERSION == '' ]]
+then
+  CURRENT_VERSION='v0.1.0'
+fi
+echo "Current Version: $CURRENT_VERSION"
+
+# replace . with space so can split into an array
+CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
+
+# get number parts
+VNUM1=${CURRENT_VERSION_PARTS[0]}
+VNUM2=${CURRENT_VERSION_PARTS[1]}
+VNUM3=${CURRENT_VERSION_PARTS[2]}
+
+if [[ $VERSION == 'major' ]]
+then
+  VNUM1=v$((VNUM1+1))
+elif [[ $VERSION == 'minor' ]]
+then
+  VNUM2=$((VNUM2+1))
+elif [[ $VERSION == 'patch' ]]
+then
+  VNUM3=$((VNUM3+1))
 else
-    echo "env file exists."
+  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  exit 1
 fi
 
-# php artisan migrate
-php artisan clear
-php artisan optimize:clear
-php artisan migrate
+# create new tag
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
-# Fix files ownership.
-chown -R www-data .
-chown -R www-data /app/storage
-chown -R www-data /app/storage/logs
-chown -R www-data /app/storage/framework
-chown -R www-data /app/storage/framework/sessions
-chown -R www-data /app/bootstrap
-chown -R www-data /app/bootstrap/cache
+# get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
-# Set correct permission.
-chmod -R 775 /app/storage
-chmod -R 775 /app/storage/logs
-chmod -R 775 /app/storage/framework
-chmod -R 775 /app/storage/framework/sessions
-chmod -R 775 /app/bootstrap
-chmod -R 775 /app/bootstrap/cache
+# only tag if no tag already
+if [ -z "$NEEDS_TAG" ]; then
+  echo "Tagged with $NEW_TAG"
+  git tag $NEW_TAG
+  git push --tags
+  git push
+else
+  echo "Already a tag on this commit"
+fi
 
-php-fpm -D
-nginx -g "daemon off;"
+echo ::set-output name=git-tag::$NEW_TAG
+
+exit 0
